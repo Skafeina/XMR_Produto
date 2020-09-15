6725849a1c29ab35f2b1b00fc8dd3c2fb82c129f
Skafeina <skafeina@gmail.com>
2020-09-14 22:22:01 -0300

    Cria layout de cadastro de produto e codifica o clique do botão cadastrar.

---
+++ XMR_Produto/Activities/CadastraProdutoActivity.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using XMR_Produto.Classes;
+using System.Collections.Generic;
+
+namespace XMR_Produto.Activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class CadastraProdutoActivity : AppCompatActivity
+    {
+        EditText edtDescricao, edtPreco, edtQuantidade;
+        Button btnCadastrar;
+
+        private List<Produto> _produtos = new List<Produto>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+           
+            // Linha que cria o vínculo entre layout (parte visual) com esta classe (codificação).
+            SetContentView(Resource.Layout.activity_cadastraProduto);
+
+            //Capturar os objetos do layout
+            edtDescricao = FindViewById<EditText>(Resource.Id.edtDescricao);
+            edtPreco = FindViewById<EditText>(Resource.Id.edtPreco);
+            edtQuantidade = FindViewById<EditText>(Resource.Id.edtQuantidade);
+            btnCadastrar = FindViewById<Button>(Resource.Id.btnCadastrar);
+
+            btnCadastrar.Click += BtnCadastrar_Click;
+
+        }
+
+        private void BtnCadastrar_Click(object sender, System.EventArgs e)
+        {
+            Produto novoProduto = new Produto
+            {
+                Descricao = edtDescricao.Text,
+                Preco = decimal.Parse(edtPreco.Text),
+                Quantidade = int.Parse(edtQuantidade.Text)
+            };
+
+            _produtos.Add(novoProduto);
+
+            edtDescricao.Text = "";
+            edtPreco.Text = "";
+            edtQuantidade.Text = "";
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XMR_Produto/Classes/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XMR_Produto.Classes
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
